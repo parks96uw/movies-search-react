7310860002362b2e1e6d530e9d16166215fdde01
Sarah Park <spark4774@gmail.com>
2020-05-08 21:57:04 -0700

    pagination part 1

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ require('dotenv').config();
 class App extends React.Component {
     state = ({
         movies: [],
-        pageNumber: 1
+        pageNumber: 1,
+        term: ''
     })
 
 
@@ -21,27 +22,38 @@ class App extends React.Component {
     //       Revise the GET request -- needs to take into account pages returned
     //       Default page to begin on -- PAGE 1
     //       Set default on APP -- callback on movielist to return new page number
-    onFormSubmit = async (term) => {
+    //       Page items is set to 20 by default
+    onFormSubmit = async (term, pageNum) => {
         const response = await tmdb.get('/search/movie', {
             params: {
                 api_key: process.env.REACT_APP_API_KEY,
                 query: term,
                 language: "EN",
                 include_adult: false,
-                page: this.pageNumber // revisit here
+                page: pageNum // revisit here
             }
         })
 
         this.setState({
-            movies: response.data.results
+            movies: response.data.results,
+            term: term
         });
+    }
+
+    onPageChange = (page) => {
+        const currPage = this.state.pageNumber;
+        this.setState({
+            pageNumber: page + currPage
+        });
+
+        this.onFormSubmit(this.state.term, this.pageNumber);
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onFormSubmit} />
-                <MovieList movies={this.state.movies} />
+                <MovieList onPageChange={this.onPageChange} term={this.state.term} movies={this.state.movies} />
             </div>
         )
     }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import MovieItem from './MovieItem';
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ term, movies }) => {
+    const [pageNumber, setPageNumber] = useState(1); // Default page is number 1
+
     const renderedList = movies.map((movie) => {
         return <MovieItem key={movie.id} movie={movie}/>;
     });
 
+
+
     // TODO: Add a loader / spinner while returning list of movies
     return ( 
-        <div className="movie-list ui four doubling cards">{renderedList}</div>
+        <div>
+            <div>{pageNumber}</div>
+            <button onClick={event => setPageNumber(pageNumber - 1)} className="ui button">Back</button>
+            <button onClick={event => setPageNumber(pageNumber + 1)} className="ui button">Next</button>
+            <div className="movie-list ui four doubling cards">{renderedList}</div>
+        </div>
     )
 }
 
